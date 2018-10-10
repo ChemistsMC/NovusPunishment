50b414aeee8edfbb8c66cf0b94c8fe3da7708bff
Evan Maddock <maddock.evan@vivaldi.net>
2018-10-10 17:01:29 -0400

    Totally forgot to check for the configured list of disallowed commands
    Signed-off-by: Evan Maddock <maddock.evan@vivaldi.net>

--- src/main/java/me/ebonjaeger/novuspunishment/listener/PlayerChatListener.java
+++ src/main/java/me/ebonjaeger/novuspunishment/listener/PlayerChatListener.java
@@ -1,6 +1,8 @@
 package me.ebonjaeger.novuspunishment.listener;
 
 import me.ebonjaeger.novuspunishment.*;
+import me.ebonjaeger.novuspunishment.configuration.ActionSettings;
+import me.ebonjaeger.novuspunishment.configuration.SettingsManager;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -15,12 +17,14 @@ public class PlayerChatListener implements Listener {
 
 	private NovusPunishment plugin;
 	private BukkitService bukkitService;
+	private SettingsManager settings;
 	private StateManager stateManager;
 
 	@Inject
-	PlayerChatListener(NovusPunishment plugin, BukkitService bukkitService, StateManager stateManager) {
+	PlayerChatListener(NovusPunishment plugin, BukkitService bukkitService, SettingsManager settings, StateManager stateManager) {
 		this.plugin = plugin;
 		this.bukkitService = bukkitService;
+		this.settings = settings;
 		this.stateManager = stateManager;
 	}
 
@@ -52,6 +56,14 @@ public class PlayerChatListener implements Listener {
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerCommand(PlayerCommandPreprocessEvent event) {
 		if (event.isCancelled()) {
+			return;
+		}
+
+		// Get the actual command; we don't care about any command arguments
+		String command = event.getMessage().substring(1).split(" ")[0];
+
+		// If the command is not in the configured list of disallowed commands, exit
+		if (!settings.getProperty(ActionSettings.DISALLOWED_COMMANDS).contains(command)) {
 			return;
 		}
 
