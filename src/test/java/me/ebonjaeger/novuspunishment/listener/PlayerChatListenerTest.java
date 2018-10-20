4a2a865524b62a7c72870caf464133c3890251b8
Evan Maddock <maddock.evan@vivaldi.net>
2018-10-19 21:54:32 -0400

    Add tests for PlayerChatListener's chat handling
    Signed-off-by: Evan Maddock <maddock.evan@vivaldi.net>

--- src/test/java/me/ebonjaeger/novuspunishment/listener/PlayerChatListenerTest.java
+++ src/test/java/me/ebonjaeger/novuspunishment/listener/PlayerChatListenerTest.java
@@ -20,6 +20,7 @@ import me.ebonjaeger.novuspunishment.StateManager;
 import me.ebonjaeger.novuspunishment.configuration.ActionSettings;
 import me.ebonjaeger.novuspunishment.configuration.SettingsManager;
 import org.bukkit.entity.Player;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.junit.Before;
 import org.junit.Test;
@@ -50,6 +51,81 @@ public class PlayerChatListenerTest {
         disallowedCommands.add("msg");
         given(settings.getProperty(ActionSettings.DISALLOWED_COMMANDS)).willReturn(disallowedCommands);
         listener = new PlayerChatListener(messenger, settings, stateManager);
+    }
+
+    @Test
+    public void chatShouldBeDisallowed() {
+        // given
+        Player player = mock(Player.class);
+        String message = "hey, who was that jerk who muted me?";
+
+        Player extra = mock(Player.class);
+        Set<Player> onlinePlayers = new HashSet<>();
+        onlinePlayers.add(player);
+        onlinePlayers.add(extra);
+
+        AsyncPlayerChatEvent event = new AsyncPlayerChatEvent(false, player, message, onlinePlayers);
+
+        given(player.getUniqueId()).willReturn(UUID.randomUUID());
+        PlayerState state = new PlayerState(player.getUniqueId(), "Allen", true, Instant.MAX);
+        given(stateManager.getPlayerState(player.getUniqueId())).willReturn(state);
+
+        // when
+        listener.onPlayerAsyncChat(event);
+
+        // then
+        assertThat(event.isCancelled(), equalTo(true));
+        verify(messenger).sendMessage(player, Message.CHAT_WHILE_MUTED);
+    }
+
+    @Test
+    public void chatShouldBeAllowedAndUnmute() {
+        // given
+        Player player = mock(Player.class);
+        String message = "hey, who was that jerk who muted me?";
+
+        Player extra = mock(Player.class);
+        Set<Player> onlinePlayers = new HashSet<>();
+        onlinePlayers.add(player);
+        onlinePlayers.add(extra);
+
+        AsyncPlayerChatEvent event = new AsyncPlayerChatEvent(false, player, message, onlinePlayers);
+
+        given(player.getUniqueId()).willReturn(UUID.randomUUID());
+        PlayerState state = new PlayerState(player.getUniqueId(), "Allen", true, Instant.MIN);
+        given(stateManager.getPlayerState(player.getUniqueId())).willReturn(state);
+
+        // when
+        listener.onPlayerAsyncChat(event);
+
+        // then
+        assertThat(event.isCancelled(), equalTo(false));
+        assertThat(state.isMuted(), equalTo(false));
+        assertThat(state.getUntil(), equalTo(null));
+        verifyZeroInteractions(messenger);
+    }
+
+    @Test
+    public void chatPlayerNotMuted() {
+        // given
+        Player player = mock(Player.class);
+        String message = "hey, that person who unmuted me was really nice!";
+
+        Player extra = mock(Player.class);
+        Set<Player> onlinePlayers = new HashSet<>();
+        onlinePlayers.add(player);
+        onlinePlayers.add(extra);
+
+        AsyncPlayerChatEvent event = new AsyncPlayerChatEvent(false, player, message, onlinePlayers);
+
+        given(player.getUniqueId()).willReturn(UUID.randomUUID());
+
+        // when
+        listener.onPlayerAsyncChat(event);
+
+        // then
+        assertThat(event.isCancelled(), equalTo(false));
+        verifyZeroInteractions(messenger);
     }
 
     @Test
@@ -105,7 +181,7 @@ public class PlayerChatListenerTest {
     }
 
     @Test
-    public void playerNotMuted() {
+    public void commandPlayerNotMuted() {
         // given
         Player player = mock(Player.class);
         String message = "/msg Bob hey, that person who unmuted me was really nice!";
